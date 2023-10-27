c45710d70983db16b00e9f19142cf275be9ea54e
vitorbezerra <vitorbezera@gmail.com>
2023-10-27 16:33:06 +0200

    frist pipline version

---
+++ youtube-collector/metadata/youtube-metadata.py
@@ -0,0 +1,87 @@
+import json
+import os
+
+from minio import Minio
+from kafka import KafkaProducer
+from googleapiclient.discovery import build
+
+
+def init_context(context):
+    client = Minio(
+        os.environ.get("MINIO_HOME"),
+        access_key=os.environ.get("MINIO_ACCESS_KEY"),
+        secret_key=os.environ.get("MINIO_SECRET_KEY"),
+        secure=False,
+    )
+
+    producer = KafkaProducer(
+        bootstrap_servers=[os.environ.get("KAFKA_BROKER")],
+        value_serializer=lambda x: json.dumps(x).encode("utf-8"),
+    )
+
+    api_key = os.environ.get("YOUTUBE_API_KEY")
+    youtube = build("youtube", "v3", developerKey=api_key)
+
+    setattr(context, "producer", producer)
+    setattr(context, "client", client)
+    setattr(context, "youtube", youtube)
+
+
+def generate_folder(video_id):
+    folder_name = []
+    # count 2 caracters to create subfolder
+    for i in range(0, len(video_id), 2):
+        # Check if the remaining characters are less than 2
+        if i + 1 == len(video_id):
+            # Read only the last character
+            char = video_id[i]
+        else:
+            # Read 2 characters at a time
+            char = video_id[i : i + 2]
+
+        folder_name.append(char)
+
+    return "/".join(folder_name)
+
+
+def handler(context, event):
+    video_id = json.loads(event.body.decode("utf-8"))
+
+    try:
+        videos_response = (
+            context.youtube.videos()
+            .list(
+                part=[
+                    "contentDetails",
+                    "id",
+                    "liveStreamingDetails",
+                    "localizations",
+                    "player",
+                    "recordingDetails",
+                    "snippet",
+                    "statistics",
+                    "status",
+                    "topicDetails",
+                ],
+                id=video_id,
+            )
+            .execute()
+        )
+
+        with open("{}.json".format("meta"), "w", encoding="utf-8") as f:
+            json.dump(videos_response, f, ensure_ascii=False, indent=4)
+
+        # Open the JSON file.
+        json_file = open("meta.json", "rb")
+
+        # Upload the JSON file to Minio in a streaming fashion.
+        object_name = "{}/{}".format(generate_folder(video_id), "meta.json")
+        context.client.fput_object(
+            "videos", object_name, json_file, content_type="application/json"
+        )
+
+        # Close the JSON file.
+        json_file.close()
+
+    except Exception as e:
+        print(e)
---
+++ youtube-collector/thumbnails/youtube-thumbnails.py
@@ -0,0 +1,75 @@
+import json
+import os
+import urllib.request
+
+from minio import Minio
+from kafka import KafkaProducer
+
+
+def init_context(context):
+    client = Minio(
+        os.environ.get("MINIO_HOME"),
+        access_key=os.environ.get("MINIO_ACCESS_KEY"),
+        secret_key=os.environ.get("MINIO_SECRET_KEY"),
+        secure=False,
+    )
+
+    producer = KafkaProducer(
+        bootstrap_servers=[os.environ.get("KAFKA_BROKER")],
+        value_serializer=lambda x: json.dumps(x).encode("utf-8"),
+    )
+
+    setattr(context, "producer", producer)
+    setattr(context, "client", client)
+
+
+def generate_folder(video_id):
+    folder_name = []
+    # count 2 caracters to create subfolder
+    for i in range(0, len(video_id), 2):
+        # Check if the remaining characters are less than 2
+        if i + 1 == len(video_id):
+            # Read only the last character
+            char = video_id[i]
+        else:
+            # Read 2 characters at a time
+            char = video_id[i : i + 2]
+
+        folder_name.append(char)
+
+    return "/".join(folder_name)
+
+
+def handler(context, event):
+    video_id = event.body.decode("utf-8")
+    bucket = "videos"
+
+    thumb_types = [
+        "default",
+        "mqdefault",
+        "sddefault",
+        "hqdefault",
+        "sddefault",
+        "maxresdefault",
+    ]
+
+    for image_types in thumb_types:
+        try:
+            image_url = f"https://img.youtube.com/vi/{video_id}/{image_types}.jpg"
+            image_name = "{}/{}/{}.jpg".format(
+                generate_folder(video_id), "thumbnails", image_types
+            )
+            image_file = urllib.request.urlopen(image_url)
+
+            context.client.put_object(
+                bucket,
+                image_name,
+                image_file,
+                length=int(image_file.headers["Content-Length"]),
+                content_type="image/jpeg",
+            )
+
+            image_file.close()
+        except Exception as e:
+            print(e)
+            continue

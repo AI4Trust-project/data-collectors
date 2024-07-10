d3ab516e8494efd2b01257da99246bb1267bf8bb
vitorbezerra <vitorbezera@gmail.com>
2024-07-10 14:10:38 +0200

    fix: repo clean up

--- pulsar/delete.sh
+++
@@ -1,7 +0,0 @@
-kubectl delete -f unifier/unifier.yaml
-kubectl delete -f hate_speech_detector/hate_speech_detector.yaml
-kubectl delete -f dynamicrouting/dynamicrouter.yaml
-kubectl delete -f thumbnaildownloader/thumbnaildownloader.yaml
-kubectl delete -f pulsar-sink/cloud-sink.yaml
-kubectl delete -f youtube_collector/youtube_collector.yaml
-kubectl delete -f data_consumer/data_consumer.yaml

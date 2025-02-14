77742ff3471ea072e41b6151aecec7ede86ab9d9
TLouf <loufthomas@gmail.com>
2025-02-14 16:34:33 +0100

    write raw data to iceberg directly and avoid filesytem writes

--- telegram-collector/messages-querier/messages-querier.py
+++ telegram-collector/messages-querier/messages-querier.py
@@ -1,4 +1,3 @@
-import base64
 import datetime
 import json
 import os
@@ -364,7 +363,6 @@ async def collect_messages(
     forwards_stats: dict[int, dict],
     linked_chans_stats: dict[str, dict],
     anon_func,
-    messages_save_path: Path,
     media_save_path: Path,
     fs,
     producer,
@@ -372,35 +370,37 @@ async def collect_messages(
     offset_id=0,
 ):
     last_id = offset_id
-    with fs.open(messages_save_path, "a") as f:
-        async for m in collegram.messages.yield_channel_messages(
-            client,
-            channel,
-            dt_from,
-            dt_to,
-            forwards_stats,
-            linked_chans_stats,
-            anon_func,
-            media_save_path,
-            offset_id=offset_id,
-            fs=fs,
-        ):
-            m_json = m.to_json()
-            f.write(m_json)
-            f.write("\n")
-
-            # MessageService have so many potential structures that putting them in a
-            # table does not make sense.
-            if not isinstance(m, MessageService):
-                m_dict = collegram.messages.to_flat_dict(m)
-                m_dict["table"] = "telegram-channel-messages"
-                m_dict["channel_id"] = channel.channel_id
-                m_dict["query_id"] = query_id
-                # send message to iceberg
-                producer.send(
-                    "telegram_collected_messages", value=iceberg_json_dumps(m_dict)
-                )
-            last_id = m.id
+    # Pass `fs` to following call to write embedded web pages as json artifacts
+    async for m in collegram.messages.yield_channel_messages(
+        client,
+        channel,
+        dt_from,
+        dt_to,
+        forwards_stats,
+        linked_chans_stats,
+        anon_func,
+        media_save_path,
+        offset_id=offset_id,
+        fs=fs,
+    ):
+        m_dict = m.to_dict()
+        m_dict["channel_id"] = channel.channel_id
+        m_dict["query_id"] = query_id
+        # MessageService have so many potential structures that putting them together
+        # with normal messages in a table does not make sense.
+        if isinstance(m, MessageService):
+            producer.send(
+                "telegram.raw_service_messages", value=iceberg_json_dumps(m_dict)
+            )
+        else:
+            producer.send(
+                "telegram.raw_messages", value=iceberg_json_dumps(m_dict)
+            )
+            m_dict = collegram.messages.flatten_dict(m, m_dict)
+            producer.send(
+                "telegram.messages", value=iceberg_json_dumps(m_dict)
+            )
+        last_id = m.id
     return last_id
 
 
@@ -460,7 +460,6 @@ def handler(context, event):
     data_path = Path("/telegram/")
     paths = collegram.paths.ProjectPaths(data=data_path)
     media_save_path = paths.raw_data / "media"
-    chan_paths = collegram.paths.ChannelPaths(channel_id, paths)
 
     full_chat_d = collegram.channels.load(channel_id, paths, fs=fs)
     chat_d = collegram.channels.get_matching_chat_from_full(full_chat_d, channel_id)
@@ -498,6 +497,7 @@ def handler(context, event):
         connection, "telegram.channels_to_query", update_d, "id"
     )
 
+    # Collect by chunks of a month to limit effects of a crash on the collection.
     dt_bin_edges = pl.datetime_range(
         dt_from, global_dt_to, interval="1mo", eager=True, time_zone="UTC"
     )
@@ -505,92 +505,75 @@ def handler(context, event):
     forwarded_chans_stats = {}
     linked_chans_stats = {}
 
-    # Caution: the sorting only works because of file name format!
-    existing_files = sorted(list(fs.glob(f"{chan_paths.messages}/*.jsonl")))
-
     for dt_from, dt_to in zip(dt_bin_edges[:-1], dt_bin_edges[1:]):
         chunk_fwds_stats = {}
         chunk_linked_chans_stats = {}
-        dt_from_in_path = dt_from.replace(
-            day=1, hour=0, minute=0, second=0, microsecond=0
-        ).date()
-        messages_save_path = (
-            chan_paths.messages / f"{dt_from_in_path}_to_{dt_to.date()}.jsonl"
-        )
-        is_last_saved_period = (
-            len(existing_files) > 0 and messages_save_path == existing_files[-1]
-        )
 
-        if not fs.exists(messages_save_path) or is_last_saved_period:
-            offset_id = 0 if last_queried_message_id is None else last_queried_message_id
+        query_time = datetime.datetime.now().astimezone(datetime.timezone.utc)
+        query_info = {
+            "query_id": str(uuid.uuid4()),
+            "query_date": query_time,
+            "data_owner": os.environ["TELEGRAM_OWNER"],
+            "channel_id": input_chat.channel_id,
+            "message_offset_id": last_queried_message_id,
+        }
 
-            query_time = datetime.datetime.now().astimezone(datetime.timezone.utc)
-            query_info = {
-                "query_id": str(uuid.uuid4()),
-                "query_date": query_time,
-                "data_owner": os.environ["TELEGRAM_OWNER"],
-                "channel_id": input_chat.channel_id,
-                "message_offset_id": offset_id,
-                "result_path": str(messages_save_path.absolute()),
-            }
-
-            context.logger.info(f"## Collecting messages from {dt_from} to {dt_to}")
-            new_offset_id = client.loop.run_until_complete(
-                collect_messages(
-                    client,
-                    input_chat,
-                    dt_from,
-                    dt_to,
-                    chunk_fwds_stats,
-                    chunk_linked_chans_stats,
-                    anonymiser.anonymise,
-                    messages_save_path,
-                    media_save_path,
-                    fs,
-                    producer,
-                    query_info["query_id"],
-                    offset_id=offset_id,
-                )
+        context.logger.info(f"## Collecting messages from {dt_from} to {dt_to}")
+        last_queried_message_id = client.loop.run_until_complete(
+            collect_messages(
+                client,
+                input_chat,
+                dt_from,
+                dt_to,
+                chunk_fwds_stats,
+                chunk_linked_chans_stats,
+                anonymiser.anonymise,
+                media_save_path,
+                fs,
+                producer,
+                query_info["query_id"],
+                offset_id=last_queried_message_id,
             )
+        )
 
-            update_d = {"id": channel_id, "last_queried_message_id": new_offset_id}
-            collegram.utils.update_postgres(
-                connection, "telegram.channels_to_query", update_d, "id"
+        update_d = {"id": channel_id, "last_queried_message_id": last_queried_message_id}
+        collegram.utils.update_postgres(
+            connection, "telegram.channels_to_query", update_d, "id"
+        )
+        # Save metadata about the query itself
+        m = json.loads(json.dumps(query_info, default=_json_default))
+        m["table"] = "telegram-queries"
+        producer.send("telegram_collected_metadata", value=m)
+
+        for fwd_id, fwd_stats in chunk_fwds_stats.items():
+            prev_stats = forwarded_chans_stats.get(fwd_id)
+            end_chunk_stats = get_new_link_stats(prev_stats, fwd_stats)
+            handle_forward(
+                channel_id,
+                fwd_id,
+                end_chunk_stats,
+                client,
+                connection,
+                distance_from_core,
+                producer,
+                lang_priorities,
             )
-            # Save metadata about the query itself
-            m = json.loads(json.dumps(query_info, default=_json_default))
-            m["table"] = "telegram-queries"
-            producer.send("telegram_collected_metadata", value=m)
+            forwarded_chans_stats[fwd_id] = end_chunk_stats
 
-            for fwd_id, fwd_stats in chunk_fwds_stats.items():
-                prev_stats = forwarded_chans_stats.get(fwd_id)
-                end_chunk_stats = get_new_link_stats(prev_stats, fwd_stats)
-                handle_forward(
-                    channel_id,
-                    fwd_id,
-                    end_chunk_stats,
-                    client,
-                    connection,
-                    distance_from_core,
-                    producer,
-                    lang_priorities,
-                )
-                forwarded_chans_stats[fwd_id] = end_chunk_stats
-
-            for link_un, link_stats in chunk_linked_chans_stats.items():
-                prev_stats = linked_chans_stats.get(link_un)
-                end_chunk_stats = get_new_link_stats(prev_stats, link_stats)
-                handle_linked_chan(
-                    channel_id,
-                    link_un,
-                    end_chunk_stats,
-                    client,
-                    connection,
-                    distance_from_core,
-                    producer,
-                    lang_priorities,
-                )
-                linked_chans_stats[link_un] = end_chunk_stats
+        for link_un, link_stats in chunk_linked_chans_stats.items():
+            prev_stats = linked_chans_stats.get(link_un)
+            end_chunk_stats = get_new_link_stats(prev_stats, link_stats)
+            handle_linked_chan(
+                channel_id,
+                link_un,
+                end_chunk_stats,
+                client,
+                connection,
+                distance_from_core,
+                producer,
+                lang_priorities,
+            )
+            linked_chans_stats[link_un] = end_chunk_stats
 
     # Send a message to call this querier again.
     m = json.loads(json.dumps({"status": "chan_message_collection_done"}))
